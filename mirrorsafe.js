a349ed4290740d614587f994aafcfc28d3e19410
newcyber <newcyber.inc@gmail.com>
2020-01-18 12:48:13 +0700

    Update mirrorsafe.js

--- mirrorsafe.js
+++ mirrorsafe.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://mirror.my.id/?site" + encryptedUrl;
+            a_to_ve[i].href = "http://mirror.my.id/safelink/?site" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
